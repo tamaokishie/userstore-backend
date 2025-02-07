3dfa0263b9a25063cc57e872f75d233530d7f7ff
tamaokishie <shie0041@gmail.com>
2025-02-07 11:48:42 +0900

    fix controller

--- src/main/java/com/tamaokishie/userstore/controller/UserstoreController.java
+++ src/main/java/com/tamaokishie/userstore/controller/UserstoreController.java
@@ -1,15 +1,42 @@
 package com.tamaokishie.userstore.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserstoreController {
     
-    @GetMapping("/user")
-    public String getUser() {
-        return "user name";
+    private final List<String> users = new ArrayList<>();
+
+    // Userを追加する
+    @PostMapping("/user")
+    public void createUser(@RequestBody User user) {
+        users.add(user.getName());
     }
     
+    // User一覧を取得する
+    @GetMapping("/users")
+    public List<String> getUser() {
+        return users;
+    }
+    
+}
+
+// ユーザーデータ用のクラス
+class User {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
